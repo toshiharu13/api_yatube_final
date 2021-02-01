cbd11014ebe482b338bf7eef1dd9ec3fcd85a913
Boyko Alexey <elorian@yandex.ru>
2021-02-01 14:07:20 +0300

    add adminka. add user for key

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from django.urls import path, include
 
 urlpatterns = [
+    path('api/v1/token/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
 ]
