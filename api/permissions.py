9a0fc2f0c974acbf6685c205eb69ca623e04ab69
Alex <elorian@yandex.ru>
2021-02-07 12:35:24 +0300

    all_done v5

--- api/permissions.py
+++ api/permissions.py
@@ -8,5 +8,6 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS or
+        return (request.method in
+                permissions.SAFE_METHODS or
                 obj.author == request.user)
