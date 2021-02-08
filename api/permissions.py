94ee035379c7a8f0526d55e535469787e690ae92
Boyko Alexey <elorian@yandex.ru>
2021-02-08 13:11:32 +0300

    rev3 all done

--- api/permissions.py
+++ api/permissions.py
@@ -2,11 +2,8 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
-    """
-    Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `owner` attribute.
-    """
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions
-                .SAFE_METHODS or obj.author == request.user)
+        return (request.method in
+                permissions.SAFE_METHODS or
+                obj.author == request.user)
